4263636cc15cb1cd000cb3f0c37ac4ad1571a42b
zss192 <zhaoshishun1@gmail.com>
2024-06-11 20:59:59 +0800

    feat: ✨ add dictionary manage

---
+++ jwb-system/jwb-system-service/src/main/java/com/jwb/system/mapper/DictionaryMapper.java
@@ -0,0 +1,15 @@
+package com.jwb.system.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.jwb.system.model.po.Dictionary;
+
+/**
+ * <p>
+ * 数据字典 Mapper 接口
+ * </p>
+ *
+ * @author itcast
+ */
+public interface DictionaryMapper extends BaseMapper<Dictionary> {
+
+}
---
+++ jwb-system/jwb-system-service/src/main/java/com/jwb/system/service/impl/DictionaryServiceImpl.java
@@ -0,0 +1,43 @@
+package com.jwb.system.service.impl;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.jwb.system.mapper.DictionaryMapper;
+import com.jwb.system.model.po.Dictionary;
+import com.jwb.system.service.DictionaryService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * <p>
+ * 数据字典 服务实现类
+ * </p>
+ *
+ * @author itcast
+ */
+@Slf4j
+@Service
+public class DictionaryServiceImpl extends ServiceImpl<DictionaryMapper, Dictionary> implements DictionaryService {
+
+    @Override
+    public List<Dictionary> queryAll() {
+        List<Dictionary> list = this.list();
+        System.out.println(list);
+        return list;
+    }
+
+    @Override
+    public Dictionary getByCode(String code) {
+
+
+        LambdaQueryWrapper<Dictionary> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(Dictionary::getCode, code);
+
+        Dictionary dictionary = this.getOne(queryWrapper);
+
+
+        return dictionary;
+    }
+}

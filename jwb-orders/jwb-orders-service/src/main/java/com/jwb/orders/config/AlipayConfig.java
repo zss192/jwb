b42a5502245a47480612110037b11e90ffa432db
zss192 <zhaoshishun1@gmail.com>
2024-07-11 11:14:27 +0800

    feat: ✨ alipay test

--- jwb-orders/jwb-orders-service/src/main/java/com/jwb/orders/config/AlipayConfig.java
+++ jwb-orders/jwb-orders-service/src/main/java/com/jwb/orders/config/AlipayConfig.java
@@ -1,10 +1,7 @@
 package com.jwb.orders.config;
 
 /**
- * @author Mr.M
- * @version 1.0
  * @description 支付宝配置参数
- * @date 2022/10/20 22:45
  */
 public class AlipayConfig {
     // 商户appid
@@ -16,11 +13,11 @@ public class AlipayConfig {
     // 页面跳转同步通知页面路径 需http://或者https://格式的完整路径，不能加?id=123这类自定义参数，必须外网可以正常访问 商户可以自定义同步跳转地址
     public static String return_url = "http://商户网关地址/alipay.trade.wap.pay-JAVA-UTF-8/return_url.jsp";
     // 请求网关地址
-    public static String URL = "https://openapi.alipaydev.com/gateway.do";
+    public static String URL = "https://openapi-sandbox.dl.alipaydev.com/gateway.do";
     // 编码
     public static String CHARSET = "UTF-8";
     // 返回格式
-    public static String FORMAT = "json";
+    public static String FORMAT = "JSON";
     // 支付宝公钥
 //	public static String ALIPAY_PUBLIC_KEY = "";
     // 日志记录目录
